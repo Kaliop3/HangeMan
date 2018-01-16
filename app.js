ddef7e936e0c413a4511b39e1e1090aba4c5b5d5
Klaudia Lasoń <klaudiaklason@gmail.com>
2018-01-16 10:21:50 +0100

    Poprawki

--- app.js
+++ app.js
@@ -16,11 +16,37 @@ console.log(hange[0]);
 for (i = 0; i < secret.length; i++) {
     line += " _";
 }
-let proba = 0;
-let podane = "";
-let wybor = "";
+let proba = 7;
+let podane = [];
+let zleZnaki = [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64];
+let dobreZnaki = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
 console.log(line);
-for (let n = 0; n < 31; n++) {
-    console.log("Pozostało Ci " + proba + "prób\nStrzały trafione" + podane + "\nProsze zgadnij litere ");
-    console.log("Twoim wyborem było : " + wybor + "");
+secretarray = secret.split("");
+let wybor = rs.question("Posze podaj litere : ");
+for (proba; proba > 0; proba--) {
+    console.log("Pozostało Ci " + proba + " prób\nStrzały trafione: " + podane + "\nProsze zgadnij litere! ");
+    console.log("Twoim wyborem było : " + wybor.toUpperCase() + "");
+    for (i = 0; i < podane.length; i++) {
+        if (wybor.charCodeAt(0) == podane[i]) {
+            console.log("Czy nie podawałeś już tej liczby ?");
+        }
+        podane.push(wybor.charCodeAt(0));
+    }
+    for (let n = 0; n < zleZnaki.length; n++) {
+        if (wybor.charCodeAt(0) == zleZnaki[n]) {
+            console.log("Nie podałeś litery alfabetu ");
+        }
+    }
+    for (let n = 0; n < dobreZnaki.length; n++) {
+        if (wybor.charCodeAt(0) == dobreZnaki[n]) {
+            for (let i = 0; i < secretarray.length; i++) {
+                if (wybor.charCodeAt(0) == secretarray[i]) {
+                    console.log("Super!");
+                }
+            }
+        }
+
+    }
+    console.log("Spróboj ponownie");
+
 }
